3bb319c30e4ab12ac5a2b91246f5bdc7d64f302b
diki-haryadi <dk.harryadi@gmail.com>
2025-04-28 11:27:21 +0700

    fix: endpoint album

--- consumer/src/services/MailSender.js
+++ consumer/src/services/MailSender.js
@@ -6,6 +6,10 @@ class MailSender {
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
       secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
     });
   }
 
--- open-music-api/src/api/albums/routes.js
+++ open-music-api/src/api/albums/routes.js
@@ -7,77 +7,47 @@ console.log('Absolute uploads path:', Path.resolve(__dirname, '../../../uploads'
 
 const routes = (handler) => [
   {
-    // add triger delete cache  if album added
     method: 'POST',
     path: '/albums',
-    handler: handler.postAlbumHandler,
-    options: {
-        auth: 'openmusic_jwt',
-    },
+    handler: handler.postAlbumHandler
   },
   {
-    // Kriteria 4 : Menerapkan Server-Side Cache
-// Menerapkan server-side cache pada jumlah yang menyukai sebuah album (GET /albums/{id}/likes).
-// Cache harus bertahan selama 30 menit.
-// Respons yang dihasilkan dari cache harus memiliki custom header properti X-Data-Source bernilai “cache”.
-// Cache harus dihapus setiap kali ada perubahan jumlah like pada album dengan id tertentu.
-// Memory caching engine wajib menggunakan Redis atau Memurai (Windows).
-// Nilai host server Redis wajib menggunakan environment variable REDIS_SERVER
     method: 'GET',
     path: '/albums/{id}',
-    handler: handler.getAlbumByIdHandler,
-    options: {
-        auth: 'openmusic_jwt',
-    },
+    handler: handler.getAlbumByIdHandler
   },
   {
     method: 'PUT',
     path: '/albums/{id}',
-    handler: handler.putAlbumByIdHandler,
-    options: {
-        auth: 'openmusic_jwt',
-    },
+    handler: handler.putAlbumByIdHandler
   },
   {
     method: 'DELETE',
     path: '/albums/{id}',
-    handler: handler.deleteAlbumByIdHandler,
-    options: {
-        auth: 'openmusic_jwt',
-    },
+    handler: handler.deleteAlbumByIdHandler
   },
   {
     method: 'POST',
     path: '/albums/{id}/likes',
     handler: handler.postAlbumLikeHandler,
-    options: {
-      auth: 'openmusic_jwt',
-    }
   },
   {
     method: 'DELETE',
     path: '/albums/{id}/likes',
     handler: handler.deleteAlbumLikeHandler,
-    options: {
-      auth: 'openmusic_jwt',
-    }
   },
   {
     method: 'GET',
     path: '/albums/{id}/likes',
     handler: handler.getAlbumLikesHandler,
-    options: {
-        auth: 'openmusic_jwt',
-    },
   },
   {
     method: 'POST',
     path: '/albums/{id}/covers',
     handler: handler.postAlbumCoverHandler,
     options: {
-      auth: 'openmusic_jwt',
       payload: {
-        maxBytes: 1024 * 1024 * 10,
+        maxBytes: 512000,
         allow: 'multipart/form-data',
         multipart: true,
         output: 'stream',
